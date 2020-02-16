97e98ee46645af1c90713c95f866892697dbcb89
sjapalucci <steve.japalucci@gmail.com>
2020-02-16 02:45:26 -0500

    Description Update

--- apsauto.sh
+++ apsauto.sh
@@ -1,10 +1,7 @@
 #! /bin/bash
 
 #################################################################################
-# Copyright (C) Steven M. Japalucci - All Rights Reserved
-# Unauthorized copying of this file, via any medium is strictly prohibited
-# Proprietary and confidential
-# Written by Steven M Japalucci RHCE RHCT <steve.japalucci@gmail.com>, August 2013
+# Written by Steven M Japalucci <steve.japalucci@gmail.com>, August 2013
 #################################################################################
 
 #Config file defining queues
