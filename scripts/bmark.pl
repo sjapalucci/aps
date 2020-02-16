18fe288eb57ca721d3e3108b5c544de17b4ad63a
sjapalucci <steve.japalucci@gmail.com>
2020-02-16 02:51:41 -0500

    Description Update

--- scripts/bmark.pl
+++ scripts/bmark.pl
@@ -1,9 +1,6 @@
 #! /usr/bin/perl
 #################################################################################
-# Copyright (C) Steven M. Japalucci - All Rights Reserved
-# Unauthorized copying of this file, via any medium is strictly prohibited
-# Proprietary and confidential
-# Written by Steven M Japalucci RHCE RHCT <steve.japalucci@gmail.com>, August 2013
+# Written by Steven M Japalucci <steve.japalucci@gmail.com>, August 2013
 #################################################################################
 
 use PDF::API2;
