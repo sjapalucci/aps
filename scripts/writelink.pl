87082c65ba48d760837fb534c62ac88431ed9737
sjapalucci <steve.japalucci@gmail.com>
2020-02-16 02:51:16 -0500

    Description Update

--- scripts/writelink.pl
+++ scripts/writelink.pl
@@ -1,9 +1,6 @@
 #!/usr/bin/perl -w
 #################################################################################
-# Copyright (C) Steven M. Japalucci - All Rights Reserved
-# Unauthorized copying of this file, via any medium is strictly prohibited
-# Proprietary and confidential
-# Written by Steven M Japalucci RHCE RHCT <steve.japalucci@gmail.com>, August 2013
+# Written by Steven M Japalucci <steve.japalucci@gmail.com>, August 2013
 #################################################################################
 use strict;
 use Spreadsheet::ParseExcel;
